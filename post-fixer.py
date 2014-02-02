bab75f4cc1c348c285038682ed32a7a72aacd19a
garrettwilkin <garrett.wilkin@gmail.com>
2014-02-02 04:35:23 +0000

    posts schema

--- post-fixer.py
+++ post-fixer.py
@@ -4,15 +4,45 @@ import json
 import sys
 
 access = json.load(open('access.json'))
+khon = pw.MySQLDatabase("khon", user=access["user"], passwd=access["password"])
+khon.connect()
+
+class BaseModel(Model):
+    class Meta:
+        database = khon
+
+class Post(BaseModel):
+    ID = BigIntegerField()
+    post_author = BigIntegerField()
+    post_date = DateTimeField()
+    post_date_gmt = DateTimeField()
+    post_content = TextField()
+    post_title = TestField()
+    post_category = IntegerField()
+    post_excerpt = TextField()
+    post_status = CharField(20)
+    comment_status = CharField(20)
+    ping_status = CharField(20)
+    post_password = CharField(20)
+    post_name = CharField(200)
+    to_ping = TextField()
+    pinged = TextField()
+    post_modified = DateTimeField()
+    post_modified_gmt = DateTimeField()
+    post_content_filtered = TextField()
+    post_parent = BigIntegerField()
+    guid = CharField(255)
+    menu_order = IntegerField()
+    post_type = CharField(20)
+    post_mime_type = CharField(100)
+    comment_count = BigIntegerField()
 
 def main():
-  print sys.argv
-  if (sys.argv[1] > 0):
-    post_id = int(sys.argv[1])
-  else:
-    print "must supply post_id."
-  khon = pw.MySQLDatabase("khon", user=access["user"], passwd=access["password"])
-  khon.connect()
+    print sys.argv
+    if (sys.argv[1] > 0):
+      post_id = int(sys.argv[1])
+    else:
+      print "must supply post_id."
 
 if __name__ == "__main__":
-  main()
+    main()
